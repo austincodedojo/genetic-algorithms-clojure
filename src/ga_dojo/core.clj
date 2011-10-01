3a4a990c4e8a41012a32645102846ef377edeb7d
Greg Symons <gsymons@gsconsulting.biz>
2011-10-01 10:35:29 -0500

    Here's what we did on 9/28...

--- src/ga_dojo/core.clj
+++ src/ga_dojo/core.clj
@@ -53,15 +53,67 @@
       '("1" "1" "1" "1") nil}
       gene))
 
+(defn decode-gene-type [gene]
+      ({'("0" "0" "0" "0") 'number
+      '("0" "0" "0" "1") 'number
+      '("0" "0" "1" "0") 'number
+      '("0" "0" "1" "1") 'number
+      '("0" "1" "0" "0") 'number
+      '("0" "1" "0" "1") 'number
+      '("0" "1" "1" "0") 'number
+      '("0" "1" "1" "1") 'number
+      '("1" "0" "0" "0") 'number
+      '("1" "0" "0" "1") 'number
+      '("1" "0" "1" "0") 'op
+      '("1" "0" "1" "1") 'op
+      '("1" "1" "0" "0") 'op
+      '("1" "1" "0" "1") 'op
+      '("1" "1" "1" "0") 'invalid
+      '("1" "1" "1" "1") 'invalid}
+      gene))
+
 
 (defn decode-reversed-chromosome [chromosome] 
-  (if (= (count chromosome) 1)
+  (if (<= (count chromosome) 1)
     (first chromosome)
     (list (second chromosome) (decode-reversed-chromosome (nnext chromosome)) (first chromosome))))
 
 (defn decode-chromosome [chromosome]
   (decode-reversed-chromosome (reverse (map decode-gene chromosome))))
 
+(defn first-is-number? [chromosome]
+  (number? (decode-gene (first chromosome))))
+
+(declare error-correct-chromosome-expecting-number)
+      
+(defn error-correct-chromosome-expecting-op [chromosome]
+  (condp = (decode-gene-type (first chromosome))
+    'number (error-correct-chromosome-expecting-op (next chromosome))
+    'op (cons (first chromosome) (error-correct-chromosome-expecting-number (next chromosome)))
+    'invalid (error-correct-chromosome-expecting-op (next chromosome))
+    nil))
+;  (cond 
+;        (< (count chromosome) 2) nil
+;        (nil? (decode-gene (first chromosome))) (error-correct-chromosome-expecting-op (next chromosome))
+;        (first-is-number? chromosome) (error-correct-chromosome-expecting-op (next chromosome))
+;        (= 0 (count (error-correct-chromosome-expecting-number (next chromosome)))) nil
+;    :default (cons (first chromosome) (error-correct-chromosome-expecting-number (next chromosome)))))
+
+
+;(defn error-correct-chromosome-expecting-op-and-number [chromosome]
+;  (let [new-chromosome (error-correct-chromosome-expecting-op chromosome)]
+;    (if (> 1 (count new-chromosome)) new-chromosome
+;      nil)))
+
+(defn error-correct-chromosome-expecting-number [chromosome]
+  (condp = (decode-gene-type (first chromosome))
+    'number (cons (first chromosome) (error-correct-chromosome-expecting-op (next chromosome)))
+    'op (error-correct-chromosome-expecting-number (next chromosome))
+    'invalid (error-correct-chromosome-expecting-number (next chromosome))
+    nil))
+
+(defn error-correct-chromosome [chromosome]
+  (error-correct-chromosome-expecting-number chromosome))
 
 (defn find-answer-for 
   "Run a genetic algorith searching for expressions that result in the number"
