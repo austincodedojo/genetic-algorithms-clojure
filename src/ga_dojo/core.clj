15fb55cebdcb8a6e1ce1955116e265a935177e75
Greg Symons <gsymons@gsconsulting.biz>
2011-10-13 01:09:42 -0500

    A first attempt at evaluation by separation.
    Basically, we looked at all the code we created so far and decided it was crap.
    So we came up with another algorithm that just assumes we can decode chromosomes
    into symbols and goes from there. Rather than error correcting the chromosome,
    we instead separate it into digits and operators, and then construct a legal
    expression. Essentially, the chromosome is now an anagram for a legal
    expression, and we construct the first one we find.

--- src/ga_dojo/core.clj
+++ src/ga_dojo/core.clj
@@ -1,124 +1,23 @@
 (ns ga-dojo.core)
 
-;; comments look like this
-
-(defn random-bit
-  "add doc-string here"
-  []
-  "1"
-  )
-
-(defn random-gene
-  "add comment here"
-  []
-  (repeat 4 (random-bit))
-  )
-
-(defn generate-chromosome 
-  "We havne't really written this yet"
-  [number]
-    (repeat number (random-gene))
-  )
-
-(defn generate-population [size]
-  (repeat size (generate-chromosome 9)))
-
-(defn mutate-bit [bit] (if (< (Math/random) 0.5) (if (= "1" bit) "0" "1") bit))
-
-(defn mutate-gene [gene] (map mutate-bit gene))
-
-(defn mutate-chromosome [chromosome] (map mutate-gene chromosome))
-
-(defn mutate-population [population] (map mutate-chromosome population))
-
-(defn evaluate-chromosome [chromosome]
-  1)
-
-(defn decode-gene [gene]
-    ({'("0" "0" "0" "0") 0
-      '("0" "0" "0" "1") 1
-      '("0" "0" "1" "0") 2
-      '("0" "0" "1" "1") 3
-      '("0" "1" "0" "0") 4
-      '("0" "1" "0" "1") 5
-      '("0" "1" "1" "0") 6
-      '("0" "1" "1" "1") 7
-      '("1" "0" "0" "0") 8
-      '("1" "0" "0" "1") 9
-      '("1" "0" "1" "0") +
-      '("1" "0" "1" "1") -
-      '("1" "1" "0" "0") *
-      '("1" "1" "0" "1") /
-      '("1" "1" "1" "0") nil
-      '("1" "1" "1" "1") nil}
-      gene))
-
-(defn decode-gene-type [gene]
-      ({'("0" "0" "0" "0") :number
-      '("0" "0" "0" "1") :number
-      '("0" "0" "1" "0") :number
-      '("0" "0" "1" "1") :number
-      '("0" "1" "0" "0") :number
-      '("0" "1" "0" "1") :number
-      '("0" "1" "1" "0") :number
-      '("0" "1" "1" "1") :number
-      '("1" "0" "0" "0") :number
-      '("1" "0" "0" "1") :number
-      '("1" "0" "1" "0") :op
-      '("1" "0" "1" "1") :op
-      '("1" "1" "0" "0") :op
-      '("1" "1" "0" "1") :op
-      '("1" "1" "1" "0") :invalid
-      '("1" "1" "1" "1") :invalid}
-      gene))
-
-
-(defn decode-reversed-chromosome [chromosome] 
-  (if (<= (count chromosome) 1)
-    (first chromosome)
-    (list (second chromosome) (decode-reversed-chromosome (nnext chromosome)) (first chromosome))))
-
-(defn decode-chromosome [chromosome]
-  (decode-reversed-chromosome (reverse (map decode-gene chromosome))))
-
-(declare advance-to-next-number)
-(declare advance-to-next-op)
-
-(defn not-a-number [gene]
-  (not (= :number (decode-gene-type gene))))
-
-(defn not-an-op [gene]
-  (not (= :op (decode-gene-type gene))))
-
-(defn advance-to-next-number [chromosome]
-  (cons advance-to-next-op (drop-while not-a-number chromosome)))
-
-(defn advance-to-next-op [chromosome]
-  (cons advance-to-next-number (drop-while not-an-op chromosome)))
-
-
-(defn drop-trailing-operators [chromosome]
-  (if (not-a-number (last chromosome))
-    (reverse (next (advance-to-next-number (reverse chromosome))))
-    chromosome))
-
-(defn error-correct-chromosome-recursively [chromosome advance]
-  (if (empty? chromosome) 
-    ()
-    (let [advance-result (apply advance [chromosome])
-          next-advance (first advance-result)
-          advanced-chromosome (next advance-result)
-         ]
-      ;(error-correct-chromosome-recursively (next advanced-chromosome) next-advance (conj result (first advanced-chromosome))))))
-      (cons (first advanced-chromosome) (error-correct-chromosome-recursively (next advanced-chromosome) next-advance)))))
-
-(defn error-correct-chromosome [chromosome]
-  (let [corrected-chromosome (error-correct-chromosome-recursively chromosome advance-to-next-number)]
-    (drop-trailing-operators corrected-chromosome)))
-
-(defn find-answer-for 
-  "Run a genetic algorith searching for expressions that result in the number"
-  [number]
-  (let [population (generate-population 100)]
-  (println population) 
-  ))
+(defn evaluate-expression [symbols]
+  (defn classify [symbol]
+    (if (number? symbol) :digits :operators))
+  (defn separate [symbols]
+    (group-by classify symbols))
+  (defn legalize-expression [symbols]
+    (defn drop-trailing-operators [expression]
+      (reverse (drop-while #(= :operators (classify %)) (reverse expression))))
+    (let [separated-symbols (separate symbols)
+          legalish-expression (cons
+                                (first (separated-symbols :digits))
+                                (interleave (separated-symbols :operators) (next(separated-symbols :digits))))]
+      legalish-expression))
+  (defn infix-to-prefix [expression] 
+    (if (<= (count expression) 2)
+      (first expression)
+      (list (second expression) (infix-to-prefix (nnext expression)) (first expression)))) 
+  (eval (infix-to-prefix (reverse (legalize-expression symbols)))))
+          
+      
+      
--- test/ga_dojo/test/core.clj
+++ test/ga_dojo/test/core.clj
@@ -2,70 +2,15 @@
   (:use [ga-dojo.core])
   (:use [clojure.test]))
 
+(deftest test-evaluate-expression
+  (testing "Expression evaluation"
+           (testing "Some valid expressions"
+                    (is (= 1 (evaluate-expression '(1))))
+                    (is (= 2 (evaluate-expression '(2))))
+                    (is (= 7 (evaluate-expression '(3 + 4))))
+                    (is (= 8 (evaluate-expression '(3 + 4 + 1))))
+                    (is (= 12 (evaluate-expression '(1 + 2 * 4))))
+)))
 
-(deftest random-gene-is-4-bits-long 
-  (is (= 4 (count (random-gene)))))
 
-(deftest random-bit-is-one-char-string
-  (is   (string? (random-bit)))
-  (is   (= 1 (count (random-bit)))))
-
-;(deftest evaluate-test-1
-;  (is (= 1 (evaluate-chromosome '((0 0 0 1))))))
-
-;(deftest evaluate-test-1
-;  (is (= 3 (evaluate-chromosome '((0 0 1 1))))))
-
-(def cs-1 '(("0" "0" "0" "1")))
-(def cs-1-plus-3 '(("0" "0" "0" "1") ("1" "0" "1" "0") ("0" "0" "1" "1") ))
-(def cs-1-plus-3-times-5 (concat cs-1-plus-3 '(("1" "1" "0" "0") ("0" "1" "0" "1"))))
-(def cs-1-plus-3-times-times-5 (concat cs-1-plus-3 '(("1" "1" "0" "0") ("1" "1" "0" "0") ("0" "1" "0" "1"))))
-(def cs-1-plus '(("0" "0" "0" "1") ("1" "0" "1" "0")))
-(def cs-1-plus-plus '(("0" "0" "0" "1") ("1" "0" "1" "0") ("1" "0" "1" "0")))
-(def cs-1-1-plus '(("0" "0" "0" "1") ("0" "0" "0" "1") ("1" "0" "1" "0")))
-(def cs-plus-1 '(("1" "0" "1" "0") ("0" "0" "0" "1")))
-(def cs-1-plus-plus-3 '(("0" "0" "0" "1") ("1" "0" "1" "0") ("1" "0" "1" "0") ("0" "0" "1" "1")))
-(def cs-1-plus-times-3 '(("0" "0" "0" "1") ("1" "0" "1" "0") ("1" "1" "0" "0") ("0" "0" "1" "1") ))
-(def cs-1-nil-plus-3 '(("0" "0" "0" "1") ("1" "1" "1" "1") ("1" "0" "1" "0") ("0" "0" "1" "1") ))
-
-(deftest decode-chromosome-cs-1-plus-3
-  (is (= (list + 1 3) (decode-chromosome cs-1-plus-3))))
-
-(deftest decode-chromosome-cs-1-plus-3-times-5
-  (is (= (list * (list + 1 3) 5) (decode-chromosome cs-1-plus-3-times-5))))
-
-(deftest decode-chromosome-cs-1-plus-3-times-5-evals-to-20
-  (is (= 20 (eval (decode-chromosome cs-1-plus-3-times-5)))))
-
-;(deftest decode-chromosome-cs-1-plus-3-with-an-error-is-1-plus-3
-;  (is (= (list + 1 3) (decode-chromosome cs-1-plus-3-with-an-error))))
-
-(deftest error-correcting-cs-1-plus-3-is-cs-1-plus-3
-  (is (= cs-1-plus-3 (error-correct-chromosome cs-1-plus-3))))
-
-(deftest error-correcting-cs-1-plus-5-is-cs-1-plus-5
-  (is (= cs-1-plus-3-times-5 (error-correct-chromosome cs-1-plus-3-times-5))))
-
-(deftest error-correcting-cs-1-plus-is-1
-  (is (= cs-1 (error-correct-chromosome cs-1-plus))))
-
-(deftest error-correcting-cs-plus-1-is-1
-  (is (= cs-1 (error-correct-chromosome cs-plus-1))))
-
-(deftest error-correcting-cs-1-plus-plus-is-1
-  (is (= cs-1 (error-correct-chromosome cs-1-plus-plus))))
-
-(deftest error-correcting-cs-1-1-plus-is-1
-  (is (= cs-1 (error-correct-chromosome cs-1-1-plus))))
-
-(deftest error-correcting-cs-1-plus-plus-3-is-cs-1-plus-3
-  (is (= cs-1-plus-3 (error-correct-chromosome cs-1-plus-plus-3))))
-
-(deftest error-correcting-cs-1-plus-3-times-times-5-is-cs-1-plus-3-times-5
-  (is (= cs-1-plus-3-times-5 (error-correct-chromosome cs-1-plus-3-times-times-5))))
-
-(deftest error-correcting-cs-1-plus-times-3-is-cs-1-plus-3
-  (is (= cs-1-plus-3 (error-correct-chromosome cs-1-plus-times-3))))
-
-(deftest error-correcting-cs-1-nil-plus-3-is-1-plus-3
-  (is (= cs-1-plus-3 (error-correct-chromosome cs-1-nil-plus-3))))
+  
